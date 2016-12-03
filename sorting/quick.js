df96136120f940d86944c9d969fd20bce2d5d9eb
kimdhoe <kimdhoe1@gmail.com>
2016-12-03 11:14:19 +0900

    quick sort

---
+++ sorting/quick.js
@@ -0,0 +1,68 @@
+const test = require('tape')
+
+// -----------------------------------------------------------------------------
+// QUICK SORT
+// -----------------------------------------------------------------------------
+
+// partition : Array<number> * int * int -> Array<number>
+// Partitions arr[p..r] using arr[r] as pivot.
+// The array arr[p..r] consists of four subarrays L, R, U, and P.
+//   - Left:    elements that comes before the pivot;  arr[p..q-1]
+//   - Right:   elements that comes sfter the pivot;   arr[q..u-1]
+//   - Unknown: elements that are not yet examined;    arr[u..r-1]
+//   - Pivot:   the pivot;                             arr[r]
+//   - q is the starting index of Right subarray.
+//   - u is the starting index of Unknown subarray.
+const partition = (arr, p, r) => {
+  // pivot : number
+  const pivot = arr[r]
+
+  // q : int
+  let q = p
+
+  for (let u = p; u < r; u++) {
+    if (arr[u] <= pivot) {
+      [ arr[q], arr[u] ] = [ arr[u], arr[q] ]
+      q++
+    }
+  }
+
+  arr[r] = arr[q]
+  arr[q] = pivot
+
+  return q
+}
+
+test('partition', t => {
+  const arr = [ 12, 7, 14, 9, 10, 11 ]
+  const q = partition(arr, 0, arr.length - 1)
+  t.equal(q, 3)
+  t.deepEqual(arr, [7, 9, 10, 11, 14, 12])
+
+  t.end()
+})
+
+// sort : Array<number> * int * int -> Array<number>
+// Given an array of numbers and indices of start and end positions,
+// quick-sorts in ascending order.
+const sort = (arr, p, r) => {
+  if (p >= r)
+    return arr
+
+  const q = partition(arr, p, r)
+
+  sort(arr, p, q - 1)
+  sort(arr, q + 1, r)
+}
+
+test('quick-sort', t => {
+  const a = [ 1 ]
+  sort(a, 0, a.length - 1)
+  t.deepEqual(a, [ 1 ])
+
+  const b = [ 12, 7, 14, 9, 10, 11 ]
+  sort(b, 0, b.length - 1)
+  t.deepEqual(b, [7, 9, 10, 11, 12, 14])
+
+  t.end()
+})
